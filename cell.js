81b3127395d7acdd2a789bb0b75f415603cfa1f7
Ognjen Vucetic <ognjenvucko@gmail.com>
2017-02-27 21:44:10 +0100

    Improved cell splitting, added merge behavior

---
+++ cell.js
@@ -0,0 +1,88 @@
+function Cell(p5, pos, radius, color, velocity) {
+
+  this.p5 = p5;
+  this.pos = pos;
+  this.radius = radius;
+  this.color = color;
+  this.force = p5.createVector();
+
+  this.vel = velocity || p5.createVector();
+
+  this.active = true;
+
+  this.update = function() {
+    this.pos.add(this.vel.add(this.force));
+    if (this.pos.x < 0 + this.radius) {
+      this.pos.x = this.radius;
+      this.vel.x = -1 * this.vel.x;
+    }
+    if (this.pos.x > this.p5.width - this.radius) {
+      this.pos.x = this.p5.width - this.radius;
+      this.vel.x = -1 * this.vel.x;
+    }
+    if (this.pos.y < 0 + this.radius) {
+      this.pos.y = this.radius;
+      this.vel.y = -1 * this.vel.y;
+    }
+    if (this.pos.y > this.p5.height - this.radius) {
+      this.pos.y = this.p5.height - this.radius;
+      this.vel.y = -1 * this.vel.y;
+    }
+    this.force.mult(0);
+    this.vel.mult(0.998);
+  }
+
+  this.applyForce = function(force) {
+    this.force.add(force);
+  }
+
+  this.show = function() {
+    this.p5.fill(this.color);
+    this.p5.ellipse(this.pos.x, this.pos.y, this.radius * 2, this.radius * 2);
+  }
+
+  this.contains = function(point) {
+    var dist = this.p5.dist(this.pos.x, this.pos.y, point.x, point.y);
+    return dist < this.radius;
+  }
+
+  this.overlap = function(other) {
+    return this.pos.dist(other.pos) < 0.75 * (this.radius + other.radius);
+  }
+
+  this.merge = function(other) {
+    other.active = false;
+    if (this.radius < other.radius) {
+      this.pos = other.pos;
+    }
+    var radiusSum = this.radius + other.radius;
+    this.vel = this.vel.mult(this.radius / radiusSum).add(other.vel.mult(this.radius / radiusSum));
+    this.radius = this.p5.sqrt(this.radius * this.radius + other.radius * other.radius);
+  }
+
+  this.mitosis = function() {
+
+    if (!this.active) return [];
+    this.active = false;
+
+    var newRadius = this.radius / this.p5.sqrt(2);
+
+    this.radius = newRadius;
+
+    var randomX = this.p5.random(-1, 1);
+    var randomY = this.p5.random(-1, 1);
+
+    var positionVector = this.p5.createVector(randomX, randomY).setMag(0.8 * this.radius);
+
+    var firstCellPos = this.pos.copy().sub(positionVector);
+    var secondCellPos = this.pos.copy().add(positionVector);
+
+    var vel = positionVector.setMag(1);
+    return [
+      new Cell(this.p5, firstCellPos, newRadius, this.color, vel.copy().mult(-1).add(this.vel)),
+      new Cell(this.p5, secondCellPos, newRadius, this.color, vel.copy().add(this.vel)),
+    ];
+
+  }
+
+}
---
+++ sketch.js
@@ -0,0 +1,79 @@
+var cells = [];
+var waves = [];
+
+var WINDOW_MARGIN = 20;
+var WAVE_FORCE_MAGNITUDE = 0.5;
+
+var sketch = function(p) {
+
+	p.setup = function() {
+		p.createCanvas(p.windowWidth - WINDOW_MARGIN, p.windowHeight - WINDOW_MARGIN);
+		cells.push(new Cell(p, p.createVector(p.width / 2, p.height / 2), 100, p.color(255, 200)));
+	}
+
+	p.draw = function() {
+		p.noStroke()
+		p.background(p.color(50, 52, 60));
+
+		cells.filter(function(cell) {
+			return cell.active;
+		}).forEach(function(cell) {
+
+			// handle merges
+			cells.filter(function(other) {
+				return other.active && other != cell;
+			}).forEach(function(other) {
+				if (other.overlap(cell)) {
+					other.merge(cell);
+				}
+			});
+
+			// apply wave force to cells
+			waves.filter(function(wave) {
+				return wave.alive && wave.contains(cell);
+			}).forEach(function(wave) {
+				cell.applyForce(cell.pos.copy().sub(wave.pos).setMag(WAVE_FORCE_MAGNITUDE));
+			});
+
+			cell.update();
+			cell.show();
+		});
+
+		waves.filter(function(wave) {
+			return wave.alive;
+		}).forEach(function(wave) {
+			wave.update();
+			wave.show();
+		});
+
+	}
+
+	p.mouseClicked = function() {
+
+		var clickedInsideCell = false;
+
+		cells.filter(function(cell) {
+			return cell.contains(p.createVector(p.mouseX, p.mouseY));
+		}).forEach(function(cell) {
+			clickedInsideCell = true;
+			// split to two cells
+			cell.mitosis().forEach(function(newCell) {
+				cells.push(newCell);
+			});
+		});
+
+		if (!clickedInsideCell) {
+			waves.push(new Wave(p, p.createVector(p.mouseX, p.mouseY)));
+		}
+
+	}
+
+}
+
+function shouldDo(probability, callback) {
+	if (Math.random() < probability) {
+		callback();
+	}
+}
+
+var app = new p5(sketch);
